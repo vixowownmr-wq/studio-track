f190140410242a9c61a6ba36af68ff23b6dd7f93
Vixowow <vixowow.nmr@gmail.com>
2026-03-29 11:27:03 -0300

    crear tablas al arrancar

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from app import create_app, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Crea las tablas si no existen
     app.run(debug=True)
