c2c76c6fc1fa49df826318a5be0253d94695a0f5
Vixowow <vixowow.nmr@gmail.com>
2026-03-31 07:38:01 -0300

    agregar aprobacion de fase

--- run.py
+++ run.py
@@ -1,9 +1,17 @@
 from app import create_app, db
+from sqlalchemy import text
 
 app = create_app()
-#hola buenas
+
 with app.app_context():
     db.create_all()
+    # Agregar columna aprobada si no existe
+    try:
+        with db.engine.connect() as conn:
+            conn.execute(text('ALTER TABLE phase ADD COLUMN aprobada BOOLEAN DEFAULT FALSE'))
+            conn.commit()
+    except Exception:
+        pass  # La columna ya existe
 
 if __name__ == '__main__':
     app.run(debug=True)
